11ef932c0080de3386282185b448c49f954911fe
l0o0gy <minamahone9620@gmail.com>
2024-05-23 15:32:08 +0300

    add change

--- src/RestaurantList.jsx
+++ src/RestaurantList.jsx
@@ -1,18 +1,25 @@
+import React, { useState } from "react";
 const RestaurantList = () => {
     const Restaurants = [
         { id: 1, name: 'KFC', location: 'Arbil' },
         { id: 2, name: 'Fried chiken', location: 'Mansour' },
         { id: 3, name: 'Super chiken', location: 'phalastin street' },
     ];
+    const [newRestaurant,setnewResturant]=useState([])
+
+    const addRestaurant = (restaurant) => {
+      setnewResturant([... Restaurants, newRestaurant]);
+    };
+
 
   return (
     <div data-testid="restaurants-list">
-                {Restaurants.map(restaurant => (
-                    <li key={restaurant.id}>
-                       name: {restaurant.name} <br/>
-                       location: {restaurant.location}
-                    </li>
-                ))}
+      {Restaurants.map(restaurant => (
+        <div key={restaurant.id}>
+          name: {restaurant.name} <br/>
+          location: {restaurant.location}
+        </div>
+        ))}
     </div>
   );
 };
